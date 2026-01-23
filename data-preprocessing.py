f7629dcc82ae32ea80464ea982bd91bb9ccdd950
AlexJ2006 <alexjakeman@sky.com>
2026-01-23 16:40:21 +0000

    Adding the dataset (only sheffield)

---
+++ data-preprocessing.py
@@ -0,0 +1,8 @@
+# Here, I will begin completing the data pre-processing functions.
+
+# Need to identify which columns need pre-processing. For this, I plan on looping round each column of the databse and seeing whether they have any n/a values.
+
+import pandas as pd
+
+
+

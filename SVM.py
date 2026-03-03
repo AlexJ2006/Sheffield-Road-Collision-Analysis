e8171a82cba14d837894ded354a153fdc358807c
AlexJ2006 <alexjakeman@sky.com>
2026-03-03 15:17:47 +0000

    Polynomial Kernel within the classifier

--- SVM.py
+++ SVM.py
@@ -43,3 +43,98 @@ y_pred = svc_linear.predict(x_test_scaled)
 
 #   Printing the accuracy of the model
 print(accuracy_score(y_test, y_pred))
+
+# import numpy as np
+# import pandas as pd # Import pandas
+
+# print("\n==============================")
+# print(" DIABETES PREDICTION SYSTEM ")
+# print("==============================\n")
+
+
+# # ---------- SAFE INPUT FUNCTION ----------
+# def get_value(prompt):
+#     while True:
+#         try:
+#             return float(input(prompt))
+#         except ValueError:
+#             print("Invalid input. Please enter a numeric value.\n")
+
+
+# # ---------- TAKE INPUT ----------
+# Pregnancies = get_value("Enter number of Pregnancies: ")
+# Glucose = get_value("Enter Glucose level: ")
+# BloodPressure = get_value("Enter Blood Pressure value: ")
+# SkinThickness = get_value("Enter Skin Thickness value: ")
+# Insulin = get_value("Enter Insulin level: ")
+# BMI = get_value("Enter BMI value: ")
+# DiabetesPedigreeFunction = get_value("Enter Diabetes Pedigree Function value: ")
+# Age = get_value("Enter Age: ")
+
+
+# # ---------- CREATE ARRAY AND CONVERT TO DATAFRAME WITH FEATURE NAMES ----------
+# user_data_array = np.array([[ # Create array first
+#     Pregnancies,
+#     Glucose,
+#     BloodPressure,
+#     SkinThickness,
+#     Insulin,
+#     BMI,
+#     DiabetesPedigreeFunction,
+#     Age
+# ]])
+
+# # Define column names based on the order of input features
+# feature_names = [
+#     'Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness',
+#     'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age'
+# ]
+
+# # Convert to DataFrame with specified column names
+# user_data = pd.DataFrame(user_data_array, columns=feature_names)
+
+
+# # ---------- SCALE INPUT ----------
+# user_data = scaler.transform(user_data)
+
+
+# # ---------- PREDICT ----------
+# prediction = svm_linear.predict(user_data)[0]
+
+
+# # ---------- OUTPUT ----------
+# print("\n==============================")
+# print(" RESULT ")
+# print("==============================")
+
+# if prediction == 1:
+#     print("Prediction: Diabetic")
+# else:
+#     print("Prediction: Not Diabetic")
+
+
+# # ---------- PROBABILITY ----------
+# if hasattr(svm_linear, "predict_proba"):
+#     prob = svm_linear.predict_proba(user_data)[0][1]
+#     print("Probability of Diabetes:", round(prob,3))
+
+# print("\nDone.")
+
+
+
+
+#Building an SVM Classifier (RBF Kernel)
+
+SVM_classifier = SVC(C=1, kernel='poly',class_weight=None, gamma='scale')
+
+SVM_classifier.fit(x_train_scaled, y_train)
+
+#   Asking for a prediction of Y
+y_pred = svc_linear.predict(x_test_scaled)
+
+#   Printing the accuracy of the model
+print(accuracy_score(y_test, y_pred))
+
+
+
+
